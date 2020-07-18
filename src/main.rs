19d795ab2adaf2ee974cd95102af4add3227348e
Charlie Brooking <cbrookin@thoughtworks.com>
2020-07-18 23:12:19 +1000

    reject using option; replace multiset with vec

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,33 @@
-use multiset::HashMultiSet;
-
 fn main() {
-    for x in get_partition().iter() {
-        println!("{}", x);
+    match get_partitions(vec![1,1,1]) {
+        None => println!("No"),
+        Some(_) => println!("Yes"),
     }
 }
 
-fn get_partition() -> HashMultiSet<i32> {
-    let mut result = HashMultiSet::new();
-    result.insert(1);
-    result.insert(2);
-    result
+fn get_partitions(xs: Vec<i32>) -> Option<Vec<Vec<i32>>> {
+    if xs.iter().sum::<i32>() % 3 != 0  {
+        return None
+    }
+    let mut partitions: Vec<Vec<i32>> = Vec::new();
+    partitions.push(vec![1]);
+    partitions.push(vec![1]);
+    partitions.push(vec![1]);
+    Some(partitions)
 }
 
 #[cfg(test)]
 mod tests {
     #[test]
-    fn exploration() {
-        let result = super::get_partition();
-        assert!(result.contains(&1));
-        assert!(result.contains(&2));
-        assert!(!result.contains(&3));
+    fn rejects_indivisible_tasks() {
+        assert_eq!(super::get_partitions(vec![1,1,2]).is_none(), true);
+    }
+
+    #[test]
+    fn partitions_three_equal_tasks() {
+        for partition in super::get_partitions(vec![1,1,1]).unwrap() {
+            assert_eq!(partition.len(), 1);
+            assert_eq!(partition[0], 1);
+        }
     }
 }
