1a64fdcc685fdf2aa8074181831a4e59d9b5bcdd
Charlie Brooking <cbrookin@thoughtworks.com>
2020-07-19 23:46:41 +1000

    slightly less ugly

--- src/main.rs
+++ src/main.rs
@@ -11,41 +11,24 @@ fn get_partitions(xs: &[u32]) -> Option<Vec<Vec<u32>>> {
         return None
     }
     let target = total / 3;
-    let p1: Vec<u32> = Vec::new();
-    let p2: Vec<u32> = Vec::new();
-    let p3: Vec<u32> = Vec::new();
-    build_partitions(xs, target, &p1, &p2, &p3)
+    build_partitions(xs, target, vec![Vec::new(); 3])
 }
 
-fn build_partitions(xs: &[u32], target: u32, p1: &[u32], p2: &[u32], p3: &[u32]) -> Option<Vec<Vec<u32>>> {
-    if p1.iter().sum::<u32>() == target && p2.iter().sum::<u32>() == target && p3.iter().sum::<u32>() == target {
-        return Some(vec![p1.to_vec(), p2.to_vec(), p3.to_vec()])
+fn build_partitions(xs: &[u32], target: u32, ps: Vec<Vec<u32>>) -> Option<Vec<Vec<u32>>> {
+    if ps.iter().all(|p| p.iter().sum::<u32>() == target) {
+        return Some(ps.to_vec())
     }
     if xs.len() == 0 {
         return None
     }
-    if p1.iter().sum::<u32>() + xs[0] <= target {
-        let mut p1x: Vec<u32> = p1.to_vec();
-        p1x.push(xs[0]);
-        let ps1 = build_partitions(&xs[1..], target, &p1x, &p2, &p3);
-        if ps1.is_some() {
-            return ps1
-        }
-    }
-    if p2.iter().sum::<u32>() + xs[0] <= target {
-        let mut p2x: Vec<u32> = p2.to_vec();
-        p2x.push(xs[0]);
-        let ps2 = build_partitions(&xs[1..], target, &p1, &p2x, &p3);
-        if ps2.is_some() {
-            return ps2
-        }
-    }
-    if p3.iter().sum::<u32>() + xs[0] <= target {
-        let mut p3x: Vec<u32> = p3.to_vec();
-        p3x.push(xs[0]);
-        let ps3 = build_partitions(&xs[1..], target, &p1, &p2, &p3x);
-        if ps3.is_some() {
-            return ps3
+    for (i, p) in ps.iter().enumerate() {
+        if p.iter().sum::<u32>() + xs[0] <= target {
+            let mut px: Vec<u32> = p.to_vec(); px.push(xs[0]);
+            let mut psx: Vec<Vec<u32>> = ps.to_vec(); psx[i] = px;
+            let result = build_partitions(&xs[1..], target, psx);
+            if result.is_some() {
+                return result
+            }
         }
     }
     None
@@ -53,20 +36,6 @@ fn build_partitions(xs: &[u32], target: u32, p1: &[u32], p2: &[u32], p3: &[u32])
 
 #[cfg(test)]
 mod tests {
-    #[test]
-    fn i_can_slice_array() {
-        let xs = [10, 20, 30];
-        assert_eq!(xs[1..], [20, 30]);
-    }
-
-    #[test]
-    fn i_can_clone_and_extend_vector() {
-        let xs: Vec<u32> = vec![10, 20];
-        let mut ys: Vec<u32> = xs.clone();
-        ys.push(30);
-        assert_eq!(ys[..], [10, 20, 30]);
-    }
-
     #[test]
     fn rejects_empty_set() {
         assert!(super::get_partitions(&[]).is_none());
